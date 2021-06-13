89c79ff56014f49fcf2630677723d7352aca8afb
gurn33sh <gurneeshchahal09@gmail.com>
2021-06-13 18:16:52 +0530

    Reddit scrapper almost complete

--- utility/image.js
+++ utility/image.js
@@ -5,6 +5,8 @@ const fetch = require('node-fetch')
 const path = require('path')
 const { url } = require('inspector')
 const { redgifExtractor } = require('./redgifs_extractor')
+const Fs = require('@supercharge/filesystem')
+const { spawn } = require('child_process')
 
 //TODO: Check if file exists, then continue
 
@@ -31,17 +33,35 @@ async function redditUrlRequest(subreddit, previd) {
     }
 }
 
+async function exists(path) {
+    try {
+        await Fs.access(path)
+        return true
+    } catch {
+        return false
+    }
+}
+
 async function redditDownloader(subreddit) {
     let FINISHED = false
     let previd = ''
     let items = []
     let count = 0
     let temp = ''
+    let json_data
+    if (await Fs.exists(subreddit)) {
+        console.log("Directory exists", subreddit)
+    } else {
+        fs.mkdirSync(subreddit)
+        fs.mkdirSync(path.join(subreddit, "images"))
+        fs.mkdirSync(path.join(subreddit, "videos"))
+        fs.mkdirSync(path.join(subreddit, "resized"))
+    }
     while (!FINISHED) {
         console.log(items.length)
         if (items.length === 0) {
             console.log('gggggggggggggggggggggggggggggggggggggggggggggggggggggg')
-            let json_data = await redditUrlRequest(subreddit, previd)
+            json_data = await redditUrlRequest(subreddit, previd)
             let data_items = json_data['data']['children']
             // console.log(items)
             Object.keys(data_items).map((key, ind) => {
@@ -60,8 +80,9 @@ async function redditDownloader(subreddit) {
             console.log('bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb')
             try {
                 temp = items[items.length - 1]['data']['id']
-                console.log('ffffffffffffffffffffff',temp)
-                let json_data = await redditUrlRequest(subreddit, previd)
+                // temp = previd
+                console.log('ffffffffffffffffffffff', temp)
+                json_data = await redditUrlRequest(subreddit, previd)
                 let data_items = json_data['data']['children']
                 Object.keys(data_items).map((key, ind) => {
                     // console.log(data_items[key])
@@ -75,66 +96,107 @@ async function redditDownloader(subreddit) {
         console.log('kkkkkkkkkkkkkkkkkkkkkkkkkk')
         console.log(count)
 
-        Object.keys(items).map((key, ind) => {
-            let item = items[key]['data']
+        for (let item of items) {
+            // console.log(item)
+            // let item = items[key]['data']
+            item = item['data']
             let link = item['url']
             console.log(link)
-            let title = item['title'].replace(/[.,\/"?#!$%\^&\*;:{}=\-_`~()]/g,"")
+            let title = item['title'].replace(/[.,\/"?#!$%\^&\*;:{}=\-_`~()]/g, "")
             let name = item['name']
             let permalink = item['permalink'].split('/')
             if (link.endsWith(".jpg") || link.endsWith('.jpeg') || link.endsWith('.png') || link.endsWith('.webp')) {
-                // let fileName = permalink[permalink.length - 2] + '(' + name + ')' + '.' + link.split('.')[link.split('.').length - 1]
-                let fileName = title + ' (' + name + ')' + '.' + link.split('.')[link.split('.').length - 1]
-                let exists = false
+                let fileName = permalink[permalink.length - 2] + '(' + name + ')' + '.' + link.split('.')[link.split('.').length - 1]
+                // let fileName = title + ' (' + name + ')' + '.' + link.split('.')[link.split('.').length - 1]
+                fileName = fileName.replace('/ /g', '_')
+                let exists = await Fs.exists(path.join(path.join(subreddit, 'images'), fileName))
+                if (exists) { console.log('jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj', exists) }
                 // fs.access(path.join('images', fileName), (err) => {
                 //     if (err === null) {
-                //         console.log("File exists", filename)
+                //         console.log('jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj')
+                //         console.log("File exists", fileName)
                 //         exists = true
                 //     }
                 // })
-                // if (exists) {continue}
-                try {
-                    fetch(link)
-                        .then((resp) => resp.arrayBuffer())
-                        .then((imgData) => {
-                            const buffer = Buffer.from(imgData);
-                            fs.writeFile(path.join('images', fileName), buffer, (err) => {
-                                // console.log('lengthhhhhhhhhhhhhhhhhhhhhhhhhh', typeof buffer)
-                                if (err) { console.log(err) }
-                                else { console.log(fileName, 'saved successfully') }
-                            })
-                        }).catch((err) => { console.log(err) })
-                    // let imgData = await resp.arrayBuffer()
+                if (!exists) {
+                    try {
+                        fetch(link)
+                            .then((resp) => resp.arrayBuffer())
+                            .then((imgData) => {
+                                const buffer = Buffer.from(imgData);
+                                fs.writeFile(path.join(subreddit, path.join('images', fileName)), buffer, (err) => {
+                                    // console.log('lengthhhhhhhhhhhhhhhhhhhhhhhhhh', typeof buffer)
+                                    if (err) { console.log(err) }
+                                    else { console.log(fileName, 'saved successfully') }
+                                })
+                            }).catch((err) => { console.log(err) })
+                            // resizeImage(subreddit, fileName)
+                        // let imgData = await resp.arrayBuffer()
 
-                } catch (err) { console.log(err) }
+                    } catch (err) { console.log(err) }
+                }
+                else {
+                    console.log("File Already present not downloaded")
+                }
             }
             else if (link.includes("redgifs")) {
-                redgifExtractor(link, name, title)
+                redgifExtractor(link, name, title, subreddit)
             }
+            // items = []
             // console.log('gggggggggggggggggggggggg', id, link)
-        })
-        console.log(temp)
-        if (count===2) {break}
+            else {
+                vidDownloader(link, path.join(subreddit, 'videos'))
+            }
+        }
+        console.log('ggggggggggggggggggggggggg', temp)
+        if (json_data['data']['after'] === null) { console.log("nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn Now after is null"); process.exit() }
+        // if (count===2) {break}
         // break
-        if (temp === previd) { FINISHED = true }
+        if (temp === previd) {
+            console.log('temp', temp)
+            console.log('previd', previd)
+            console.log("Completly scrapped")
+            FINISHED = true
+            // process.exit()
+            // break
+        }
     }
     // console.log(items)
 }
 
-function resizeImage() {
-    const fileName = "lp.jpg"
+async function vidDownloader(url, videos_path) {
+    console.log("Yyyyyyyyyyyyyyyyyyyy", 'youtube-dl in da house')
+    const ydl = spawn('youtube-dl', ['-o', path.join(videos_path, '%(id)s.%(ext)s'), url])
+
+    ydl.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+
+    ydl.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
+    ydl.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
+}
+
+function resizeImage(subreddit, fileName) {
+    // fileName = fileName.replace(/(\s+)/g, '\\$1')
+    // console.log(escape(fileName))
+    // process.exit()
+    const dir_path = path.join(subreddit, 'resized')
     console.log("Resizing...")
-    const stats = fs.statSync(fileName)
+    const stats = fs.statSync(path.join(path.join(subreddit, 'images'), fileName))
     console.log("Size of " + fileName + "is: ", stats['size'] * 0.001 + " KB")
 
-    fs.readFile(fileName, async (err, data) => {
+    fs.readFile(path.join(path.join(subreddit, 'images'), fileName), async (err, data) => {
         await sharp(data)
             .resize({ width: 400, height: 400, fit: 'fill' })
             .jpeg({ quality: 80 })
             .toBuffer()
             .then((data) => {
-                fs.writeFile("resized.jpg", data, (err) => {
-                    console.log(typeof data)
+                fs.writeFile(path.join(dir_path, fileName), data, (err) => {
                     console.log(data.length * 0.001 + " KB")
                     if (err) { console.log("Error accured", err) }
                     else { console.log("Image is resized and saved as", "resized.jpg") }
@@ -144,4 +206,5 @@ function resizeImage() {
 }
 
 // resizeImage()
-redditDownloader('kaylanilei')
+redditDownloader('wallpapers')
+// vidDownloader("https://www.redgifs.com/watch/digitalblackarmyworm", 'lp')
--- utility/redgifs_extractor.js
+++ utility/redgifs_extractor.js
@@ -1,30 +1,44 @@
-const fetch = require('node-fetch') 
+const fetch = require('node-fetch')
 const path = require('path')
 const fs = require('fs')
+const Fs = require('@supercharge/filesystem')
 
-async function downloader(name, url, reddit_name, reddit_title) {
-    reddit_title = reddit_title.replace(/[.,\/"?#!$%\^&\*;:{}=\-_`~()]/g,"")
+async function downloader(name, url, reddit_name, reddit_title, subreddit) {
+    reddit_title = reddit_title.replace(/[.,\/"?#!$%\^&\*;:{}=\-_`~()]/g, "")
+    let fileName = path.join(subreddit, path.join('videos', name + '_' + reddit_name + '_' + reddit_title + '.mp4'))
+    console.log(fileName)
+    let exists = await Fs.exists(fileName)
+    if (exists) { console.log('lllllllllllllllllllllllllllllllllllll', exists) }
     fetch(url)
-    .then( (resp) => resp.arrayBuffer())
-    .then( (data) => {
-        const buffer = Buffer.from(data)
-        fs.writeFile(path.join('videos', name + '_' + reddit_name + '_' + reddit_title  + '.mp4'), buffer, (err) => {
-            if (err) {console.log(err)}
-            else {console.log("redgif saved successfully")}
+        .then((resp) => resp.arrayBuffer())
+        .then((data) => {
+            const buffer = Buffer.from(data)
+            // fs.access(path.join('videos', fileName), (err) => {
+            //     if (err === null) {
+            //         console.log('jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj')
+            //         console.log("File exists", fileName)
+            //         exists = true
+            //     }
+            // })
+            if (!exists) {
+                fs.writeFile(fileName, buffer, (err) => {
+                    if (err) { console.log(err) }
+                    else { console.log("redgif saved successfully") }
+                })
+            }
         })
-    })
 }
 
-async function redgifExtractor(url, reddit_name, reddit_title) {
+async function redgifExtractor(url, reddit_name, reddit_title, subreddit) {
     url = url.split('/')
-    const name = url[url.length -1]
-    console.log(url[url.length -1])
+    const name = url[url.length - 1]
+    console.log(url[url.length - 1])
     const api_url = "https://api.redgifs.com/v1/gfycats/" + name
     try {
-    const resp = await fetch(api_url)
-    const json_data = await resp.json()
-    const mp4Url = (json_data['gfyItem']['mobileUrl'])
-    await downloader(name, mp4Url, reddit_name, reddit_title)
+        const resp = await fetch(api_url)
+        const json_data = await resp.json()
+        const mp4Url = (json_data['gfyItem']['mobileUrl'])
+        await downloader(name, mp4Url, reddit_name, reddit_title, subreddit)
     } catch (err) {
         console.log(err)
     }
